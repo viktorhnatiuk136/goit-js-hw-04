b3a85e44fd6a740e8db635eaa2dd4bccf8f2b394
viktor hnatiuk <viktorgnatiuk136@gmail.com>
2025-12-12 13:14:03 +0200

    hw4 js

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,39 @@
-function formatMessage(message, maxLength) { 
-    if (message.length <= maxLength) { 
-        return message
-    } else return message.slice(0, maxLength)+"..."
-}
+function calcAverageCalories(days) {
+    let totalCalories = 0;
+    let averageCalories = 0;
 
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+    if (days.length === 0) return averageCalories;
+    
+        for (let day of days) { 
+        totalCalories += day.calories
+    }
+        return totalCalories / days.length;
+}
+   
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
+
+console.log(
+  calcAverageCalories([])
+); // 0
